48f450d5849e09346f5c871c8bcd8fb83ed2d4e3
Amadeusz Wieczorek <git@amadeusw.com>
2018-01-03 19:51:15 -0800

    tweaks the code to receive tweets UNTIL specific ID

--- collector/index.js
+++ collector/index.js
@@ -1,3 +1,7 @@
+// USage: for all tweets until specific ID,
+// node .\index.js 940726622650490900 
+// for all tweets starting with specific ID, see commented out invocation to requestTweets
+
 var OAuth2 = require('oauth').OAuth2;
 var https = require('https');
 var fs = require('fs');
@@ -6,12 +10,12 @@ var twitterConsumerKey = process.env.twitterBridgeKey;
 var twitterConsumerSecret = process.env.twitterBridgeSecret;
 var AccessToken;
 var LastId;
-var requestCount = 1000;
-var lastIdFromParameter;
+var SinceId;
+var idFromParameter;
 
 console.log(process.argv);
 if (process.argv.length == 3)
-    lastIdFromParameter = process.argv[2];
+    idFromParameter = process.argv[2];
 
 var oauth2 = new OAuth2(
     twitterConsumerKey,
@@ -26,17 +30,19 @@ oauth2.getOAuthAccessToken(
     function (e, accessToken, refreshToken, results){
         console.log('access: ',accessToken);
         AccessToken = accessToken;
-        if (lastIdFromParameter == undefined)
-            requestTweets();
-        else
-            requestTweets(lastIdFromParameter);
+
+        requestTweets(); // use this line to request all tweets since now
+        //requestTweets(941044213935890400); // use this line to request tweets older than this ID
     }
 );
 
-var requestTweets = function(lastId) {
+var requestTweets = function(id) {
     var path = '/1.1/statuses/user_timeline.json?screen_name=SDOTbridges&count=50'
-    if (lastId != undefined) {
-        path = '/1.1/statuses/user_timeline.json?screen_name=SDOTbridges&count=50&max_id='+lastId
+    if (id != undefined) {
+        // Use the line below for tweets older than id
+        path = '/1.1/statuses/user_timeline.json?screen_name=SDOTbridges&count=50&max_id='+id
+        // Use the line below for tweets newer than id, but this is all messed up so don't. Just keeping it for API reference.
+        //path = '/1.1/statuses/user_timeline.json?screen_name=SDOTbridges&count=50&since_id='+id
     }
     console.log("Requesting", path)
     var options = {
@@ -53,46 +59,49 @@ var requestTweets = function(lastId) {
             buffer += data;
         });
         result.on('end', function () {
-            console.error(buffer);
             var tweets = JSON.parse(buffer);
-            console.log("Processing...");
             processTweets(tweets);
         });
     }).on("error", function(e){
-        console.log("Error: ", e.message);
+        console.error("Error: ", e.message);
     });
 }
 
 var processTweets = function(tweets) {
-    //console.log(tweets);
-    tweets.forEach(tweet => {
+    for(var i = 0; i < tweets.length; i++) {
+        var tweet = tweets[i];
         if (tweet.id == undefined) throw 'Invalid data'
-        //console.log("id: ", tweet.id)
-        LastId = tweet.id
-        //console.log("text", tweet.text)
-        //console.log("time ", tweet.created_at)
+        if (tweet.id > SinceId || SinceId == undefined)
+            SinceId = tweet.id
+        if (tweet.id < LastId || LastId == undefined)
+            LastId = tweet.id
+
         // subtract 8 hours
         var utcCreated = new Date(tweet.created_at)
         var localTime = new Date(utcCreated.getTime() - utcCreated.getTimezoneOffset() * 60000)
-        //console.log("local time", localTime)
         var nameLength = tweet.text.indexOf(" has ");
         var name = tweet.text.substring(0, nameLength);
         var timeIndex = tweet.text.indexOf(" to traffic - ");
         var tweetedTime = tweet.text.substring(timeIndex + 14);
         var opened = tweet.text.substring(nameLength + 5, timeIndex);
         var closedBool = opened == "closed";
-        //console.log(name, opened, opened == "reopened", tweetedTime)
-        console.log(name, opened, "at", localTime);
         var data = tweet.id + "," + name + "," + closedBool + "," + localTime.toISOString() + "," + tweetedTime + "\n"
-        fs.appendFile("tweets.csv", data, function (err) {
-            if (err) throw err;
-            console.log(data);
-        });
-    });
-    if (requestCount-- > 0) {
-        console.log("Requesting more. LastId", LastId)
-        requestTweets(LastId);
+
+        //console.log(data); // Diagnostics
+        if (tweet.id <= idFromParameter)
+        {
+            console.log("STOP! Reached ", idFromParameter, " in tweet #", tweet.id)
+            setTimeout(function() {
+                console.log('Waiting...');
+            }, 1000);
+            return;
+        }
+
+        fs.appendFileSync("tweets.csv", data);
     }
+
+    console.log("Requesting more. Id", LastId)
+    requestTweets(LastId);
 }
 var fetchData = function(accessToken) {
     var options = {
@@ -113,6 +122,6 @@ var fetchData = function(accessToken) {
             processTweets(tweets);
 });
     }).on("error", function(e){
-      console.log("Got error: " + e.message);
+      console.error("Got error: " + e.message);
     });
 }
