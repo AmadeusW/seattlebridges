c4339fddf7c24de4e3059526379a28c6d5c13dfe
Amadeusz Wieczorek <git@amadeusw.com>
2018-02-04 23:52:33 -0800

    fetches weather data from last time we had data until now

--- collector/weatherData/getItAll.py
+++ collector/weatherData/getItAll.py
@@ -1,7 +1,7 @@
-from getWeather import go
+from getWeather import go, goForwards
 import time
 
-for i in range(0, 200):
+for i in range(0, 21):
     print("iteration {0}".format(i))
-    go()
+    goForwards()
     time.sleep(6)
--- collector/weatherData/getWeather.py
+++ collector/weatherData/getWeather.py
@@ -23,21 +23,43 @@ def getEarliestData(path):
         for filename in filenames:
             if (filename.endswith("."+dataExtension)):
                 earliest = filename
-                break
+                break # stop on the first date we see. Assume that the OS returned files in sorted fashion
     year = earliest[0:2]
     month = earliest[2:4]
     day = earliest[4:6]
     return [year, month, day]
 
-def getNextDate(year, month, day):
+def getLatestData(path):
+    for (dirpath, dirnames, filenames) in os.walk(path):
+        for filename in filenames:
+            if (filename.endswith("."+dataExtension)):
+                latest = filename
+                # don't break. Try to process all files. Assume that the OS returned files in sorted fashion
+    year = latest[0:2]
+    month = latest[2:4]
+    day = latest[4:6]
+    return [year, month, day]
+
+def getPreviousDate(year, month, day):
     thisDate = datetime.strptime("{0}{1}{2}-12:00:00".format(year, month, day),
     "%y%m%d-%H:%M:%S")
     nextDate = thisDate - timedelta(days=1)
     return [nextDate.strftime("%y"), nextDate.strftime("%m"), nextDate.strftime("%d")]
 
+def getNextDate(year, month, day):
+    thisDate = datetime.strptime("{0}{1}{2}-12:00:00".format(year, month, day),
+    "%y%m%d-%H:%M:%S")
+    nextDate = thisDate + timedelta(days=1)
+    return [nextDate.strftime("%y"), nextDate.strftime("%m"), nextDate.strftime("%d")]
+
 def go():
     earliestDate = getEarliestData(dataLocation)
-    nextDate = getNextDate(earliestDate[0], earliestDate[1], earliestDate[2])
+    nextDate = getPreviousDate(earliestDate[0], earliestDate[1], earliestDate[2])
+    getWeatherData(nextDate[0], nextDate[1], nextDate[2])
+
+def goForwards():
+    latestDate = getLatestData(dataLocation)
+    nextDate = getNextDate(latestDate[0], latestDate[1], latestDate[2])
     getWeatherData(nextDate[0], nextDate[1], nextDate[2])
 
 go()
