93f601e9dfb4439d2f6fef8baf55de776da80ab5
Amadeusz Wieczorek <git@amadeusw.com>
2018-03-27 11:52:47 -0700

    some refactoring, fresh data

--- weather/getItAll.py
+++ weather/getItAll.py
@@ -1,7 +1,7 @@
 from getWeather import go, goForwards
 import time
 
-for i in range(0, 21):
+for i in range(0, 50): # the upper bound is number of days since last available piece of data and today
     print("iteration {0}".format(i))
     goForwards()
     time.sleep(6)
--- weather/getWeather.py
+++ weather/getWeather.py
@@ -4,7 +4,7 @@ import json
 from datetime import datetime, timedelta
 
 apiKeyLocation = r'C:\Users\ama\OneDrive\config\wunderground.json'
-dataLocation = "collector/weatherData/"
+dataLocation = "data/"
 dataExtension = "json"
 
 def getApiKey():
@@ -19,22 +19,30 @@ def getWeatherData(year, month, day):
     testfile.retrieve(url, dataLocation + "{0}{1}{2}.{3}".format(year, month, day, dataExtension))
 
 def getEarliestData(path):
+    earliest = ""
     for (dirpath, dirnames, filenames) in os.walk(path):
         for filename in filenames:
             if (filename.endswith("."+dataExtension)):
                 earliest = filename
                 break # stop on the first date we see. Assume that the OS returned files in sorted fashion
+    if earliest == "":
+        raise Exception("Could not find any {0} files in {1}".format(dataExtension, path))
+
     year = earliest[0:2]
     month = earliest[2:4]
     day = earliest[4:6]
     return [year, month, day]
 
 def getLatestData(path):
+    latest = ""
     for (dirpath, dirnames, filenames) in os.walk(path):
         for filename in filenames:
             if (filename.endswith("."+dataExtension)):
                 latest = filename
                 # don't break. Try to process all files. Assume that the OS returned files in sorted fashion
+    if latest == "":
+        raise Exception("Could not find any {0} files in {1}".format(dataExtension, path))
+
     year = latest[0:2]
     month = latest[2:4]
     day = latest[4:6]
@@ -62,4 +70,4 @@ def goForwards():
     nextDate = getNextDate(latestDate[0], latestDate[1], latestDate[2])
     getWeatherData(nextDate[0], nextDate[1], nextDate[2])
 
-go()
+#go()
